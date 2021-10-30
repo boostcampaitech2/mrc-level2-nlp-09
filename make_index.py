45c174f11dda91ecc348321597b2c66a0807d7f5
Jay-Ppark <godjaehyung@gmail.com>
2021-10-30 14:16:35 +0000

    solve error on make_index.py

--- make_index.py
+++ make_index.py
@@ -1,14 +1,24 @@
 import pandas as pd
 import sys
 
-qg_df = pd.read_csv('/opt/ml/code/merge_wiki_qg_id.tsv',delimiter='\t')
+qg_df = pd.read_csv('/opt/ml/code/merge_wiki_qg_id.tsv',delimiter='\t',header=None)
 qg_df.columns = ['questions','answer','document_id']
 wiki_df = pd.read_json("/opt/ml/data/merge_wiki.json")
+#print(qg_df.head(5))
 
+answers = []
+new_docs = []
 for answer, d_id in zip(qg_df['answer'],qg_df['document_id']):
-    print(answer,d_id)
+    #print(answer,d_id)
+    new_answer = {}
     docs = wiki_df.loc['text'][d_id]
     start_idx = docs.find(answer)
-    print(start_idx)
-    print(docs[start_idx:start_idx+len(answer)])
-    sys.exit()
+    new_answer['answer_start']=[start_idx]
+    new_answer['text']=[answer]
+    answers.append(new_answer)
+    new_docs.append(docs)
+    #print(start_idx)
+    #print(docs[start_idx:start_idx+len(answer)])
+    
+output_df = pd.DataFrame({'context' : new_docs, 'question' : qg_df['questions'], 'answers' : answers, 'document_id' : qg_df['document_id']})
+output_df.to_csv("qgtrain.csv",mode='w')
